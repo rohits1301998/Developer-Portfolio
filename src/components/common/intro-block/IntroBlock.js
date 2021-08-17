f96aa74224b5195f3e3a9ffda49b84924df0e830
Rohit Singh <rohit.singh@zycus.com>
2021-08-18 04:07:50 +0530

    project init

---
+++ src/components/common/intro-block/IntroBlock.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class IntroBlock extends React.Component {
+
+
+    render() {
+        return (
+        <h1>
+            Hello, Rohit
+        </h1>
+        )
+    }
+}
+
+export default IntroBlock;
